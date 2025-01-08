a6869648a86f74f703b8ba0d2d2a381ce5318144
rkarande <rkarande@galaxe.com>
2025-01-08 12:31:20 +0530

    2nd commit

--- src/main/java/com/example/demoForJenkis/DemoForJenkisApplication.java
+++ src/main/java/com/example/demoForJenkis/DemoForJenkisApplication.java
@@ -1,6 +1,5 @@
 package com.example.demoForJenkis;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
