bfdc848d862d2cde6c69b50d2f9332c7d62ed914
rkarande <rkarande@galaxe.com>
2025-01-08 11:01:29 +0530

    first commit for java

---
+++ src/main/java/com/example/demoForJenkis/DemoForJenkisApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoForJenkis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForJenkisApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hi its for jenkins");
+		//SpringApplication.run(DemoForJenkisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoForJenkis/DemoForJenkisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoForJenkis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForJenkisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
